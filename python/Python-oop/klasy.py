704a4f9bc6ae32dde3f41aa7dd48931814e221a1
SUSEK228 <148262852+SUSEK228@users.noreply.github.com>
2025-04-20 14:31:43 +0200

    dziedziczenie i abstrakcja

--- python/Python-oop/klasy.py
+++ python/Python-oop/klasy.py
@@ -1,11 +1,88 @@
-#Ćwiczenie programowania obiektowego w pythonie
-from car import Car
+#klasy
+from abc import ABC , abstractmethod
 class Car:
-    def __init__(self, model, year, color, for_sale): # za chwile wyjasnie
+    
+    wheels = 4
+    ilosc = 0
+    przebieg=0
+    
+    def __init__(self, model, year, color, for_sale): # konstrutor
         self.model=model
         self.year=year
         self.color=color
         self.for_sale=for_sale
-car1 = Car("A6", 2020, "black", False)
-car2 = Car("Mustang", 1995, "red", True)    # przypisywanie
-print(car1.model)   # wyswietlewnie 
+        Car.ilosc +=1
+        
+    def drive(self):
+        print(f"Prowadzisz {self.color} {self.model}")
+        Car.przebieg +=10
+        
+    def stop(self):
+        print(f"zatrzymales {self.color} {self.model}")
+        
+    def opis(self):
+        print(f"Samochod to: {self.model}\nrok: {self.year}\nkolor: {self.color}\nDostepny: {self.for_sale}")
+
+class Animal:
+    def __init__(self,name):
+        self.name = name
+    def eat(self):
+        print(f"{self.name} is eating")
+        
+    def sleep(self):
+        print(f"{self.name} is sleeping")
+        
+class Dog(Animal):
+    def sound(self):
+        print("Hau")
+
+class Cat(Animal):
+    def sound(self):
+        print("Miau")    
+
+
+class  Predator(Animal):
+    def hunt(self):
+        print("This animal is Predator")
+
+class Prey(Animal):
+    def flee (self):
+        print("This animal is fleeing")
+        
+class Ryba(Prey, Predator):
+    pass
+
+
+# klasa abstrakcyjna
+class Vehicle(ABC):
+    
+    #@abstractmethod
+    def go(self):
+        pass
+    
+    #@abstractmethod
+    def stop(self):
+        pass
+    
+class Motorbike(Vehicle):
+    def go(self):
+        print("You drive a MotorBike")
+    def stop(self):
+        print("You stopped a Motorbike")
+
+class Bus(Vehicle):
+    def go(self):
+        print("You drive a Bus")
+    def stop(self):
+        print("You stopped a Bus")
+
+
+class Osoba:
+    def __init__(self,imie,nazwisko):
+        self.imie=imie
+        self.nazwisko=nazwisko
+
+class Student:
+    def __init__(self,imie,nazwisko,uczelnia):
+        super().__init__(imie,nazwisko)
+        self.uczelnia=uczelnia
---
+++ python/Python-oop/main.py
@@ -0,0 +1,36 @@
+#Ćwiczenie programowania obiektowego w pythonie
+
+#from klasy import Car
+#from klasy import Dog
+#from klasy import Cat
+#from klasy import Ryba
+from klasy import Vehicle
+from klasy import Motorbike
+'''
+car1 = Car("A6", 2020, "black", False)
+car2 = Car("Mustang", 1995, "red", True)    # przypisywanie
+#car1.drive()
+#car1.drive()
+#car1.drive()
+#car2.stop()
+#print(Car.wheels)
+#print(Car.przebieg)
+#car1.opis()
+
+'''
+'''
+dog = Dog("Pies")
+kotek = Cat("Kicia")
+print(dog.name)
+dog.eat()
+kotek.sleep()
+dog.sound()
+kotek.sound()
+'''
+'''
+rybka = Ryba("Okon")
+rybka.hunt()
+rybka.eat()
+'''
+vehicle = Motorbike()
+vehicle.stop()
