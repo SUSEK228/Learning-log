a989c03e2a4efbc6619189afd66321917ab25c81
SUSEK228 <148262852+SUSEK228@users.noreply.github.com>
2025-05-12 23:55:40 +0200

    aktualizacja

--- django/blog/myapp/views.py
+++ django/blog/myapp/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User, auth 
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
@@ -13,3 +15,26 @@ def counter(request):
     text=request.POST['text']
     amount_of_words=len(text.split())
     return render(request, 'counter.html', {'amount' : amount_of_words})
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'This Email is already used')
+                return redirect('register')
+            elif User.objects.filter(username=username).exists():
+                messages.info('This username is already used')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+                return redirect('login')
+        else:
+            messages.info(request, 'Your passwords are not the same')
+            return redirect('register')
+    else:
+        return render(request, 'register.html')
